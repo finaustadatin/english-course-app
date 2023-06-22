6e597e86a49b85b6f9afdcaad3e9394aebb6ebbd
finaustadatin <finaustadatin0101@gmail.com>
2023-06-23 06:01:31 +0700

    Read and Delete

--- app/src/main/java/tech/rdirg/englishcourseapp/FirstFragment.kt
+++ app/src/main/java/tech/rdirg/englishcourseapp/FirstFragment.kt
@@ -1,11 +1,15 @@
 package tech.rdirg.englishcourseapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import tech.rdirg.englishcourseapp.application.CourseApp
 import tech.rdirg.englishcourseapp.databinding.FragmentFirstBinding
 
 /**
@@ -18,6 +22,15 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var applicationContext: Context
+    private val courseViewModel: CourseViewModel by viewModels {
+        CourseViewModelFactory((applicationContext as CourseApp).repository)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        applicationContext = context.applicationContext
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,8 +45,28 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val adapter = CourseListAdapter{ course ->
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(course)
+            findNavController().navigate(action)
+        }
+        binding.dataRecyclerView.adapter = adapter
+        binding.dataRecyclerView.layoutManager = LinearLayoutManager(context)
+        courseViewModel.allCourses.observe(viewLifecycleOwner) { courses ->
+            courses.let {
+                if (courses.isEmpty()){
+                    binding.notFoundImageView.visibility = View.VISIBLE
+                    binding.notFoundTextView.visibility = View.VISIBLE
+                }else{
+                    binding.notFoundImageView.visibility = View.GONE
+                    binding.notFoundTextView.visibility = View.GONE
+                }
+                adapter.submitList(courses)
+            }
+        }
+
         binding.addFAB.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(null)
+            findNavController().navigate(action)
         }
     }
 
--- app/src/main/java/tech/rdirg/englishcourseapp/SecondFragment.kt
+++ app/src/main/java/tech/rdirg/englishcourseapp/SecondFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import tech.rdirg.englishcourseapp.application.CourseApp
 import tech.rdirg.englishcourseapp.databinding.FragmentSecondBinding
 import tech.rdirg.englishcourseapp.model.Course
@@ -27,6 +28,9 @@ class SecondFragment : Fragment() {
         CourseViewModelFactory((applicationContext as CourseApp).repository)
     }
 
+    private val args: SecondFragmentArgs? by navArgs()
+    private var course: Course? = null
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         applicationContext = context.applicationContext
@@ -45,12 +49,23 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        course = args?.course
+        if (course != null){
+            binding.deleteButton.visibility = View.VISIBLE
+            binding.saveButton.text = "Update"
+        }
+
         val title = binding.nameEditText.text
         val category = binding.categoryEditText.text
         val content = binding.contentTextMultiLine.text
         binding.saveButton.setOnClickListener {
             val course = Course(title = title.toString(), category = category.toString(), content = content.toString())
             courseViewModel.insert(course)
+            findNavController().popBackStack()
+        }
+
+        binding.deleteButton.setOnClickListener {
+            courseViewModel.delete(course!!)
             findNavController().popBackStack()
         }
     }
